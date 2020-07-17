940c11f55cd3a994bb238165735858c223dc4237
Stephen Wright <stephenwright@Stephens-MBP-2.broadband>
2020-07-17 15:08:08 +0100

    fix puppeteer

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ const puppeteer = require('puppeteer');
 
 const buzzfeedCrawl = async () => {
   const quizzes = [];
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto('https://www.buzzfeed.com/trending/quizzes');
 
